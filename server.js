37718301d61e50c79be85bb64e64b80e7d472ee3
Germano Kuerten <germanokuerten@Germanos-MBP.lan>
2022-04-04 23:16:15 -0700

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -15,5 +15,13 @@ app.get("/greeting/:name", (req, res) => {
     res.send(`Hello, ${(req.params.name)}`)
 })
 
+/////////////////////
+// Tip Calculator
+/////////////////////
+
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    res.send(`${parseInt(req.params.total) * parseInt(req.params.tipPercentage) / 100}`)
+})
+
 app.listen(PORT, () => {
 })
